a5a847a1228427181bcada59ce7a52339ba15fc7
Daniel <danie.kratochvill@impaction.at>
2020-02-27 13:42:31 +0100

    --

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "translate3d(0,0,0)";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "translate3d(0,-100%,0)";
   }
   prevScrollpos = currentScrollPos;
 }
