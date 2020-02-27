86174b0b6875cde62408ebece47d5056a2fc8733
Daniel <danie.kratochvill@impaction.at>
2020-02-27 13:37:26 +0100

    -

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ var mobilemenu = document.getElementById("mobilemenu")
 var open = false;
 history.scrollRestoration = 'manual';
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 document.onclick = function(){
 
     if (open == true){
