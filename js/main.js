6e3bc7337cbe87300d601018c6cba919ffda4bba
Daniel <danie.kratochvill@impaction.at>
2020-02-27 16:09:25 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ history.scrollRestoration = 'manual';
 
 
 var prevScroll  = 0;
-var threshold = 110;
+var threshold = 50;
 
 
 var navhideshow = setInterval(function() {
@@ -20,7 +20,7 @@ var navhideshow = setInterval(function() {
     navbar.style.opacity = "0";
   }
 prevScroll = currentScroll;
-}, 150);
+}, 100);
 
 
 
