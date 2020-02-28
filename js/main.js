890321836f3d01d42269ab2c204facdf412c2335
Daniel <danie.kratochvill@impaction.at>
2020-02-28 13:58:45 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ var slider = document.getElementById("scroll");
 slider.addEventListener('mousedown', (e) => {
 console.log("clicked");
 mouseclicked = true;
-slider.scrollBy(100,100);
+slider.scrollLeft += 10;
 });
 
 slider.addEventListener('mouseup', (e) => {
