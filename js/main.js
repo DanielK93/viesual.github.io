346a03c7e11bf36af59139fea25e3e9786930618
Daniel <danie.kratochvill@impaction.at>
2020-02-28 14:08:38 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,10 +46,10 @@ slider.addEventListener('mousemove', e => {
     newpos = e.clientX;
     if (newpos > lastpos){
       console.log("rechts");
-      slider.scrollBy(0,10);
+      slider.scrollLeft += 100;
     } else if(newpos < lastpos){
       console.log("links");
-      slider.scrollBy(0,-10);
+      slider.scrollLeft -= 100;
     }
   lastpos = newpos;
 }
