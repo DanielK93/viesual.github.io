614189fdbe91fb6770765c02f5525035e784c4f5
Daniel <danie.kratochvill@impaction.at>
2020-02-28 11:36:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,31 +22,14 @@ var navhideshow = setInterval(function() {
 prevScroll = currentScroll;
 }, 150);
 
-const slider = document.getElementById("scroll");
-var isDown;
-let startX;
-let scrollLeft;
+var slider = document.getElementById("scroll");
+
+xpos = e.clientX;
+console.log(xpos);
 
 slider.addEventListener("mousedown", e => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = x - startX;
-  slider.scrollLeft = scrollLeft - walk;
+
+
 });
 
 document.onclick = function(){
