fcfee24b0b6a414ca545b0131d44db1290f28162
Bytebezirk <github@bytebezirk.at>
2020-03-06 20:23:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ var FadeTransition = Barba.BaseTransition.extend({
           cover.classList.add("move-up");
            
             cover.addEventListener("animationend", function(){
-              cover.style.bottom ="-100vh"
+              cover.style.top ="100vh"
                 cover.classList.remove("move-up");
                 _this.done();
 
