e5b70f5c6821eac87c70495f9ba876af29000646
Bytebezirk <github@bytebezirk.at>
2020-03-21 16:47:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,11 +32,11 @@ var latestKnownScrollY = 0;
 
 function smaller(){
 neuerwert = (latestKnownScrollY - eingabemin)/(eingabemax-eingabemin) * (neuemax-neuemin) + neuemin;
-logo.style.flexBasis = neuerwert+"%";
+logo.style.width = neuerwert+"%";
 }
 
 function fixed(){
-  logo.style.flexBasis = "10%";
+  logo.style.width = "10%";
   }
 
 window.addEventListener('scroll', ()=> {
