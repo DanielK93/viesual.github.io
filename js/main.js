230f0fdf0c724e05724a3c29512f9ef3cf057062
DanielK93 <daniel@kratochvill.at>
2020-04-15 15:20:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,19 @@ var menuoverlay = document.getElementById("menuoverlay");
 history.scrollRestoration = 'manual';
 isseen = false;
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
+// Get the header
+var header = document.getElementById("fixedheader");
+
+function myFunction() {
+  if (window.pageYOffset >= 140) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
 var switchmenu = function() {
   open = !open;
