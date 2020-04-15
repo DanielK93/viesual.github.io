e04b9ccf64527c9c1e911d50faa8d00b220ee4e3
DanielK93 <daniel@kratochvill.at>
2020-04-15 15:09:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,22 @@ var menuoverlay = document.getElementById("menuoverlay");
 history.scrollRestoration = 'manual';
 isseen = false;
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
+// Get the header
+var header = document.getElementById("fixedheader");
+
+// Get the offset position of the navbar
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= 140) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
 var switchmenu = function() {
   open = !open;
